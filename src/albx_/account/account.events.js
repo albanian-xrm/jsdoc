70e22b64ce5f92b9c644d6d6606d5970e79ff76c
Betim Beja <11160171+BetimBeja@users.noreply.github.com>
2022-09-18 11:50:30 +0200

    Added @types/xrm and JSDoc comments

--- src/albx_/account/account.events.js
+++ src/albx_/account/account.events.js
@@ -1,10 +1,16 @@
 var AlbanianXrm = window.AlbanianXrm || {};
-(function(AlbanianXrm){
+(function (AlbanianXrm) {
     AlbanianXrm.account = AlbanianXrm.account || {};
-    (function(account){
-        account.onLoad = function(context){
+    (function (account) {
+        /**
+         * OnLoad handler for the Account form
+         * @param {Xrm.Events.EventContext} context 
+         */
+        account.onLoad = function (context) {
             var formContext = context.getFormContext();
-            var name = formContext.getAttribute("name");
+            /** @type {Xrm.Attributes.StringAttribute} */
+            var name =  formContext.getAttribute("name");
+            console.log(name.getValue)
         }
     })(AlbanianXrm.account);
 })(AlbanianXrm);
